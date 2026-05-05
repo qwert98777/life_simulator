f64144ca033aaff40a7bca4614e423d9d844a4a9
qwert98777 <399kmy@gmail.com>
2026-05-05 18:15:44 +0300

    config_loader.py

--- main.py
+++ main.py
@@ -1,23 +1,10 @@
-from ecosystem import Ecosystem
-from organism import Organism
+import json
+import os
 
-def main():
-    eco = Ecosystem()
-    rabbit = Organism("Заяц", 25, is_predator=False)
-    fox = Organism("Лиса", 30, is_predator=True)
-    eco.add_organism(rabbit)
-    eco.add_organism(fox)
+class Configuration:
+    def __init__(self, path = "life-simulator\config.json"):
+        with open(path, "r", encoding="utf-8") as f:
+            self.data = json.load(f)
 
-    days = 5
-    for day in range(1, days + 1):
-        print(f"\nДень {day}")
-        eco.simulate_day()
-
-        if sum(1 for o in eco.organisms if o.is_alive()) == 0:
-            print("Все организмы погибли. Симуляция завершена.")
-            break
-    else:
-        print(f"\nСимуляция завершена. Выжившие организмы: {[o.name for o in eco.organisms if o.is_alive()]}")
-
-if __name__ == "__main__":
-    main()
+    def get(self, section, key):
+        return self.data[section][key]
