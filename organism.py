2fe45f435d0e056bf53602738dd30a7c7ec7a40b
qwert98777 <399kmy@gmail.com>
2026-05-05 18:21:43 +0300

    organism.py

---
+++ organism.py
@@ -0,0 +1,14 @@
+class Organism:
+    """Базовый класс организма."""
+    def init(self, name: str, energy: float) -> None:
+        self.name = name
+        self.energy = energy
+
+    def eat(self, food_energy: float) -> None:
+        """Метод питания."""
+        self.energy += food_energy
+        print(f"{self.name} поел и получил {food_energy} энергии.")
+
+    def is_alive(self) -> bool:
+        """Проверка состояния жизни."""
+        return self.energy > 0
