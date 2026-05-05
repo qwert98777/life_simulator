a97b9b62b1e61b55d9e58304829b72befe769e33
qwert98777 <399kmy@gmail.com>
2026-05-05 18:11:38 +0300

    config.json

--- ecosystem.py
+++ ecosystem.py
@@ -1,39 +1,12 @@
-from organism import Organism
-
-class Ecosystem:
-    def __init__(self):
-        self.organisms = []
-
-    def add_organism(self, organism: Organism) -> None:
-        self.organisms.append(organism)
-
-    def simulate_day(self) -> None:
-        print("\n--- Новый день ---")
-        new_organisms = []
-
-        for org in self.organisms[:]:
-            if not org.is_alive():
-                print(f"{org.name} мёртв и удалён.")
-                self.organisms.remove(org)
-                continue
-
-            if org.is_predator:
-                prey = next((o for o in self.organisms if not o.is_predator and o.is_alive()), None)
-                if prey:
-                    print(f"{org.name} (хищник) охотится на {prey.name}!")
-                    prey.energy -= 15
-                    if not prey.is_alive():
-                        print(f"  {prey.name} погиб.")
-                        self.organisms.remove(prey)
-                    org.eat(15)
-                else:
-                    org.eat(5)
-            else:
-                org.eat(8)
-
-            baby = org.reproduce(f"детёныш_{org.name}")
-            if baby:
-                new_organisms.append(baby)
-                print(f"  {org.name} произвёл потомство: {baby.name}")
-
-        self.organisms.extend(new_organisms)
+{
+    "simulation_settings": {
+        "start_day": 1,
+        "max_day": 10,
+        "energy_consumption" : 5.0
+    },
+    "organism_defaults": {
+        "rabbit_initial_energy": 20.0,
+        "fox_initial_energy": 30.0,
+        "plant_initial_energy": 10.0
+    }
+}
