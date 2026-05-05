5af698915dd35703ca990658c2af8da6fbe944e8
qwert98777 <399kmy@gmail.com>
2026-05-05 18:24:36 +0300

    utils.py

--- utils.py
+++ utils.py
@@ -1,1 +1,9 @@
+import random
 
+
+events = [None, "Засуха", "Высокая урожайность", "Дождь", "Плохая погода"]
+chances = [65, 5, 5, 18, 7]
+
+def event():
+    """Выбирает случайное событие из доступных"""
+    return random.choices(events, weights=chances, k=1)[0]
