ffea30a57aa8d8bb4b38b7617985c2d37e5003cf
qwert98777 <399kmy@gmail.com>
2026-05-06 18:10:53 +0300

    ecosystem.py

--- ecosystem.py
+++ ecosystem.py
@@ -1,21 +1,21 @@
 from organism import Organism
-from config_loader import Configuration
+from config_loader import load_config
 
 class Ecosystem:
-    """ Класс симулирующий экосистему """
-    def __init__(self):
-        self.config = Configuration
-        self.consumption = self.config.get("simulation", "energy_consumption")
+    """Класс, симулирующий экосистему."""
+    
+    def __init__(self, config_path: str = "config.json") -> None:
+        self.config = load_config(config_path)
         self.organisms = []
 
-    """ Добавление организма в экосистему """
-    def add_organism(self, organism: Organism):
+    def add_organism(self, organism: Organism) -> None:
+        """Добавление организма в экосистему."""
         self.organisms.append(organism)
 
-    """ Симуляция дня, проверка жив ли организм """ 
-    def simulate_day(self):
+    def simulate_day(self) -> None:
+        """Симуляция дня, проверка жив ли организм."""
         for org in self.organisms:
             if org.is_alive():
-                org.eat(-self.consumption)
+                org.eat(self.config.get("food_energy", 10))
             else:
                 print(f"{org.name} мёртв.")
