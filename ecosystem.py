b4344fee1e28aba60c25c2e06c492eef8bacec8b
qwert98777 <399kmy@gmail.com>
2026-05-07 18:16:53 +0300

    ecosystem.py

--- ecosystem.py
+++ ecosystem.py
@@ -1,19 +1,18 @@
 from organism import Organism
-from config_loader import load_config
+from predator import Predator
 
 class Ecosystem:
     """Класс, симулирующий экосистему."""
-    
-    def __init__(self, config_path: str = "config.json") -> None:
-        self.config = load_config(config_path)
+
+    def __init__(self) -> None:
         self.organisms = []
 
     def add_organism(self, organism: Organism) -> None:
-        """Добавление организма в экосистему."""
+        """Добавление организма."""
         self.organisms.append(organism)
 
     def simulate_day(self) -> None:
-        """Симуляция дня, проверка жив ли организм."""
+        """Симуляция одного дня."""
         for org in self.organisms:
             if org.is_alive():
                 org.eat(10)
