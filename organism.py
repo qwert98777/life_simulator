071ae2b70c6cca35480ba4a9f2e11f8db4d9fae2
qwert98777 <399kmy@gmail.com>
2026-05-07 18:29:09 +0300

    organism.py

--- organism.py
+++ organism.py
@@ -1,21 +1,15 @@
 class Organism:
     """Базовый класс организма."""
-    
-    def __init__(self, name: str, energy: float, is_predator: bool = False) -> None:
+
+    def __init__(self, name: str, health: float) -> None:
         self.name = name
-        self.energy = energy
-        self.is_predator = is_predator
+        self.health = health
 
-    def eat(self, food_energy: float) -> None:
+    def eat(self, food_health: float) -> None:
         """Метод питания."""
-        self.energy += food_energy
-        print(f"{self.name} поел и получил {food_energy} энергии.")
-
-    def take_damage(self, damage: float) -> None:
-        """Метод получения урона."""
-        self.energy -= damage
-        print(f"{self.name} получил {damage:.1f} урона.")
+        self.health += food_health
+        print(f"{self.name} поел и получил {food_health} здоровья.")
 
     def is_alive(self) -> bool:
         """Проверка состояния жизни."""
-        return self.energy > 0
+        return self.health > 0
